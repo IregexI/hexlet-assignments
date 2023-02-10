2f50a56d0ea05fbe266ff957a690267505aace4d
@hexlet/cli <support@hexlet.io>
2023-02-10 14:48:06 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,39 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
+  def setup; end
+
+  def teardown; end
   
+  def test_power_assert_push
+    arr = Stack.new
+    arr.push!("newfile")
+    arr_push = ["newfile"]
+    assert { arr.to_a == arr_push }
+  end
+
+  def test_power_assert_pop
+    arr = Stack.new
+    arr.push!("new")
+    arr.pop!
+    arr_pop = []
+    assert { arr.to_a == arr_pop }
+  end
+  
+  def test_power_assert_clear
+    arr = Stack.new
+    arr.push!("new")
+    arr.clear!
+    arr_clear = []
+    assert { arr.to_a == arr_clear }
+  end
+
+  def test_power_assert_empty
+    arr = Stack.new
+    arr_empty = true 
+    assert { arr.empty? == true }
+  end
+   
   # END
 end
 
