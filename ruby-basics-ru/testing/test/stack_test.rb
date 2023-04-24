68db2299835c3c0ae4dfe413cf97300aafd56b73
@hexlet/cli <support@hexlet.io>
2023-04-24 20:21:31 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -1,43 +1,35 @@
 # frozen_string_literal: true
 
+require 'minitest-power_assert'
 require_relative 'test_helper'
 require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  def setup; end
-
-  def teardown; end
-  
   def test_power_assert_push
-    arr = Stack.new
-    arr.push!("newfile")
-    arr_push = ["newfile"]
-    assert { arr.to_a == arr_push }
+   test_stack = []
+   stack_pushing = [2]
+   assert{ test_stack.push(2) == stack_pushing }
   end
 
   def test_power_assert_pop
-    arr = Stack.new
-    arr.push!("new")
-    arr.pop!
-    arr_pop = []
-    assert { arr.to_a == arr_pop }
+    test_stack = [2]
+    stack_pop = []
+    test_stack.pop
+    assert{ test_stack == stack_pop }
   end
-  
+
   def test_power_assert_clear
-    arr = Stack.new
-    arr.push!("new")
-    arr.clear!
-    arr_clear = []
-    assert { arr.to_a == arr_clear }
+    test_stack = [2]
+    stack_clear = []
+    assert{ test_stack.clear == stack_clear }
   end
 
   def test_power_assert_empty
-    arr = Stack.new
-    arr_empty = true 
-    assert { arr.empty? == true }
-  end
-   
+    test_stack = []
+    stack_is_empty = true
+    assert{ test_stack.empty? == stack_is_empty }
+  end  
   # END
 end
 
