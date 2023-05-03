be98d32d952fc6c7644e88ceb011568cf8b2329a
@hexlet/cli <support@hexlet.io>
2023-05-03 15:30:49 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -1,36 +1,50 @@
-require 'uri'
+# frozen_string_literal: true
+
+# BEGIN
 require 'forwardable'
+require 'uri'
 
 class Url
-  include Comparable
   extend Forwardable
 
-  def_delegators :@uri, :host, :scheme, :port
+  attr_reader :query_params
 
-  def initialize(url)
-    @uri = URI.parse(url)
-    print @uri
-    query = @uri.query || ''
+  def initialize(add)
+    @scheme = add[/[http|s]+/]
+    @host = add[/\/+[a-z0-9.]*/][2..]
+    @port = add[/:[0-9]+ ?/]
+    @query = add[/\?.*/]
     
-    @params = query
-              .split('&')
-              .each_with_object({}) do |query_parts, acc|
-                key, value = query_parts.split '='
-                acc[key.to_sym] = value
-              end
+    if @port.nil?
+      @port = nil
+    else
+      @port = @port[1..]
+    end
+
+    if @query.nil?
+      @query = nil
+    else
+      @query = @query[1..]
+    end
+
+    @query_params = transform(@query)
+    @address = URI::Generic.build(scheme: @scheme, host: @host, query: @query, port: @port)
   end
 
-  def query_params
-    @params
+  def_delegators :@address, :scheme, :host, :port, :query
+
+  def transform(val)
+    return {} if val.nil?
+    val.split('&').map { |param| param.split('=') }.each_with_object({}) { |param, acc| acc[param.first.to_sym] = param.last }
   end
 
-  def query_param(key, default = nil)
-    @params.fetch(key, default)
+  def query_param(key, val_default = nil)
+    value_key = @query_params[key]
+    value_key.nil? ? val_default : value_key
   end
 
-  def <=>(other)
-    [scheme, host, port, query_params] <=> [other.scheme, other.host, other.port, other.query_params]
+  def == (address)
+    ( port == address.port ) && ( host == address.host ) && ( scheme == address.scheme ) && ( query_params == address.query_params )
   end
 end
-
 # END
